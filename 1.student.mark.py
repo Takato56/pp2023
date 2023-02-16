8b28f971ddf9d269cf4a81a7257747aec7c1b062
unknown <lan.05062003@gmail.com>
2023-02-16 15:04:10 +0700

    Trying to use this

---
+++ 1.student.mark.py
@@ -0,0 +1,7 @@
+nstudent = int(input("Input the number of student: "))
+ncourse = int(input("Input the number of course: "))
+studentid = list[nstudent]
+studentname = list[nstudent]
+studentdob = list[nstudent]
+courseid = list[ncourse]
+coursename = list[ncourse]
